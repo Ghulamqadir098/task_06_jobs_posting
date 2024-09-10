e1294029210289551114e723e4e23b623e0cf60c
Ghulam Qadir <idontknow131422@gmail.com>
2024-09-10 14:20:45 +0500

    Added frontend admin panel

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,21 @@
+<div class="sidebar">
+    <nav class="nav-menu">
+        <ul>
+            <li><a href="#">Home</a></li>
+
+
+{{-- @if (Auth::user()->roles->contains('name','admin')) --}}
+    
+       
+<li class="dropdown">
+    <a href="#">Task</a>
+    <ul class="dropdown-menu">
+        <li><a href="#">Create Task</a></li>
+    </ul>
+</li>
+{{-- @endif    --}}
+
+            <li><a href="#">Logout</a></li>
+        </ul>
+    </nav>
+</div>
